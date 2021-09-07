2cb92abf2598d59a64d5587598e97221ac3561b5
Silas Gyger <silasgyger@gmail.com>
2021-09-07 12:21:29 +0200

    made edges flash

--- src/graph/edges.js
+++ src/graph/edges.js
@@ -1,7 +1,9 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getBezierPath } from 'react-flow-renderer';
+import { globalScheduler } from '../scheduler/scheduler';
 import { globalAudioGraph } from './audio';
 import { globalBangGraph } from './bang';
+
 
 const CustomPath = ({sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition, style, id}) => {
     const d = getBezierPath({sourceX, sourceY, sourcePosition, targetX, targetY, targetPosition});
@@ -15,6 +17,16 @@ export const AudioEdge = ({selected, ...props}) => {
 
 
 export const BangEdge = ({selected, ...props}) => {
+    // Connect input & output
     useEffect(() => globalBangGraph.connectNodes(props.sourceHandleId, props.targetHandleId), []);
-    return <CustomPath {...props} style={{strokeWidth: 3, fill: "none", stroke: selected ? "#555" : "#cccc"}} />
+
+    // make edge flash on bang
+    const [flash, setFlash] = useState(false);
+    const doFlash = () => {setFlash(true); setTimeout(() => setFlash(false), 100)};
+    const scheduleFlashDraw = time => globalScheduler.scheduleDraw(doFlash, time);
+    const edgeIdentifier = `edge(${props.sourceHandleId}-${props.targetHandleId})`;
+    useEffect(() => globalBangGraph.registerInputNode(edgeIdentifier, scheduleFlashDraw), []);
+    useEffect(() => globalBangGraph.connectNodes(props.sourceHandleId, edgeIdentifier), [])
+    const color = flash ? "orange" : (selected ? "#555" : "#ccc");
+    return <CustomPath {...props} style={{strokeWidth: 3, fill: "none", stroke: color}} />
 }
